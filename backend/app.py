7451cf293d7066ae31019b43e02b59b03449a2ff
Siddhartha Chakrabarty <csiddhartha2004@gmail.com>
2024-08-01 01:18:06 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
-from flask_pymongo import PyMongo
 from werkzeug.security import generate_password_hash, check_password_hash
 from moviepy.editor import VideoFileClip, AudioFileClip, concatenate_videoclips
 import os
