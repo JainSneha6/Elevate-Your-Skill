22c3ab4cc4dae51419048a853c4707ac0367cef4
mr-Prathmesh-sonawane <prathmesh.2730@gmail.com>
2024-08-01 11:42:54 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,10 @@
+import os
+import sys
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 from flask_pymongo import PyMongo
 from werkzeug.security import generate_password_hash, check_password_hash
 from moviepy.editor import VideoFileClip, AudioFileClip, concatenate_videoclips
-import os
-import sys 
 import google.generativeai as genai
 from PIL import Image
 import regex as re
